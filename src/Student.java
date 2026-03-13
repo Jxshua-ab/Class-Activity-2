7a7bc3dd46c055255eed7ac2156b3d64d0dd76e0
Jxshua-ab <joshuaabishua@gmail.com>
2026-03-13 09:06:40 +0300

    Initial commit

---
+++ src/Student.java
@@ -0,0 +1,17 @@
+public class Student extends Person {
+    private String course;
+
+    public Student(String id, String name, String email, String course) {
+        super(id, name, email);
+        this.course = course;
+    }
+
+    public String getCourse() { return course; }
+    public void setCourse(String course) { this.course = course; }
+
+    @Override
+    public void displayDetails() {
+        System.out.println("Student: " + getId() + "  " + getName() +
+                "  " + getEmail() + "  Course: " + course);
+    }
+}
